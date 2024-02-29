b28eca91f1b250f8902956ee6d9edebeb3db6130
Amine derbal <aminedoran31@gmail.com>
2024-02-29 15:18:32 +0100

    Create initial home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script>
+
+import {useUserStore} from '@/stores/userStore'
+import {onMounted,computed} from 'vue'
+import { useRouter } from 'vue-router' 
+
+
+
+
+export default {
+  setup() {
+    const userStore = useUserStore()
+    const isAdmin =  computed(() => userStore.isAdmin)
+    const router = useRouter()
+
+    onMounted(() => {
+   if(!isAdmin.value){
+   router.push('/login')
+   }
+    })
+ 
+}}
+</script>
+
+<template>
+  <h1 >Hello Home</h1>
+</template>
