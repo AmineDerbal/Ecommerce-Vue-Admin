4f73bc3b0736274cbfd1165dc81b4e3cda8cdec6
Amine derbal <aminedoran31@gmail.com>
2024-02-29 15:17:33 +0100

    Create the default App view

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import { RouterView } from 'vue-router'
+export default {
+  components: {
+    RouterView
+  }
+}
+</script>
+
+<template>
+ <div>
+  <v-container>
+   <RouterView />
+  </v-container>
+ </div>
+</template>
