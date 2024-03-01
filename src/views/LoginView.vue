46c2c618b948f98ff681a405a85b824f1abd2372
Amine derbal <aminedoran31@gmail.com>
2024-03-01 17:17:44 +0100

    Create login view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,54 @@
+<script>
+import { useUserStore } from '@/stores/userStore';
+import { computed,ref } from 'vue';
+import { useRouter } from 'vue-router'
+
+export default {
+
+  setup() {
+    const router = useRouter()
+    const userStore = useUserStore()
+    const user = ref('')
+    const password = ref('') 
+   const isAdmin =  computed(() => userStore.user.isAdmin)
+
+   const handleSubmit = async() => {
+
+     await userStore.loginAdmin(user.value, password.value)
+
+     if(isAdmin.value){
+      router.push('/')
+     }
+
+   }
+
+    return {
+      handleSubmit,
+      user,
+      password
+    }
+  }
+
+}
+</script>
+
+<template>
+  <div class="mt-10 ">
+<v-container class="text-center md:w-50 h-screen">
+  <v-row justify="center">
+    <v-col cols="12" md="8" sm="6" class="h-100">
+      <v-avatar :style="{ backgroundColor: 'red' }" >
+        <v-icon icon="$lock" color="white" ></v-icon>
+      </v-avatar>
+        <div class="text-h3 mt-4 h-sceen">Login</div>
+        <v-form @submit.prevent>
+          <v-text-field v-model="user" label="username" type="text" prepend-icon="$account" placeholder="admin" required></v-text-field>
+          <v-text-field v-model="password" label="Password" type="password" prepend-icon="$lock" placeholder="admin" required></v-text-field>
+          <v-btn @click="handleSubmit" type="submit" class="w-full" color="primary">Login</v-btn>
+        </v-form>
+    </v-col>
+  </v-row>
+</v-container>
+  </div>
+
+</template>
