5e32a78776211e56e29f905df19aee29b0cf259d
Amine derbal <aminedoran31@gmail.com>
2024-03-01 17:17:19 +0100

    Updaate user store

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,14 +1,52 @@
 import { defineStore } from 'pinia'
+import axios from 'axios'
+import { ref, watch } from 'vue'
 
-export const useUserStore = defineStore({
-  id: 'user',
-  state: () => ({
+export const useUserStore = defineStore('user', () => {
+  const user = ref({
     username: null,
-    emai: null,
+    email: null,
     isAdmin: false,
-    accessToken: null,
-    isLoading: false,
-    isError: false
-  }),
-  actions: {}
+    accessToken: null
+  })
+  const isLoading = ref(false)
+  const isError = ref(false)
+  if (localStorage.getItem('user')) {
+    user.value = JSON.parse(localStorage.getItem('user'))
+  }
+
+  watch(
+    user,
+    (userVal) => {
+      localStorage.setItem('user', JSON.stringify(userVal))
+    },
+    {
+      deep: true
+    }
+  )
+
+  const loginAdmin = async (username, password) => {
+    ;(isLoading.value = true), (isError.value = false)
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}auth/login`, {
+        username,
+        password
+      })
+      user.value.username = response.data.username
+      user.value.email = response.data.email
+      user.value.isAdmin = response.data.isAdmin
+      user.value.accessToken = response.data.accessToken
+    } catch (error) {
+      isError.value = true
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  return {
+    user,
+    isLoading,
+    isError,
+    loginAdmin
+  }
 })
