57c1056f311fa8d094a14ac389a6e94f4f9344f0
Amine derbal <aminedoran31@gmail.com>
2024-03-02 17:15:03 +0100

    Create Layout component

---
+++ src/components/layout/LayoutView.vue
@@ -0,0 +1,20 @@
+<script>
+import { RouterView } from 'vue-router'
+import TopBar from './TopBar.vue'
+import SideBar from './SideBar.vue'
+export default {
+  components: {
+    RouterView,
+    TopBar,
+    SideBar
+  }
+}
+</script>
+
+<template>
+  <TopBar />
+  <div class="flex">
+    <SideBar />
+    <RouterView />
+  </div>
+</template>
