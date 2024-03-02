fba90783804ec01066a38e853f0a2d8cf3fdd534
Amine derbal <aminedoran31@gmail.com>
2024-03-02 17:13:53 +0100

    Handle auth routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { HomeView, LoginView } from '@/views'
+import { useUserStore } from '@/stores/userStore'
+import { LayoutView } from '@/components'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      component: LayoutView,
+      meta: {
+        requiresAuth: true
+      },
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: HomeView
+        }
+      ]
+    },
+    {
       path: '/login',
       name: 'login',
       component: LoginView
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
     }
   ]
+})
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore()
+  if (to.meta.requiresAuth && !userStore.user.isAdmin) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
 })
 
 export default router
