c0a87164e8080d5fce84f684159615d5792a4984
Amine derbal <aminedoran31@gmail.com>
2024-03-02 17:14:49 +0100

    Create sidebar component

---
+++ src/components/layout/SideBar.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <div class="h-screen bg-gray-100 sticky mt-4">
+    <div class="p-[20px] text-[#555]">
+      <div class="mb-[10px]">
+        <h3 className="text-xs text-gray-500">Dashboard</h3>
+        <ul class="list-none p-[5px]">
+          <router-link to="/"> <li class="p-[5px] cursor-pointer flex items-center rounded-[10px]">
+            <v-icon icon="$home" color="black" ></v-icon>Home
+          </li></router-link>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+</template>
