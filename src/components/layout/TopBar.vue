bf9d1fc323a41c81a0f314f7b2236fcdd0e13825
Amine derbal <aminedoran31@gmail.com>
2024-03-03 15:25:57 +0100

    update display using vuetify components

--- src/components/layout/TopBar.vue
+++ src/components/layout/TopBar.vue
@@ -11,30 +11,23 @@ export default {
   }
 }
 
-
 </script>
 
 <template>
-<div class="w-full h-[80px] bg-white sticky top-0 z-50">
-  <div class="h-full px-[20px] flex items-center justify-between">
-    <div class="h-20 w-20">
-
-      <img :src="logo" alt="logo" class="cursor-pointer w-full h-full object-contain">
-    </div>
-
-    <div class="text-3xl font-bold text-gray-800"><p>
-      Admin Side
-    </p></div>
-    <div>
-      <div class="flex items-center">
-        <div class="relative cursor-pointer mr-[10px] text-[#555]">
-
-          <v-icon icon="$bellOutline" color="black" ></v-icon>
-          <span class="absolute w-[15px] h-[15px] bg-red-500 text-white text-[10px] rounded-full top-[-5px] right-0 flex justify-center items-center">2</span>
+  <v-app-bar height="80" >
+    <v-toolbar color="white" >
+      <template color="white" class="h-full w-full px-[20px] flex items-center justify-between"> 
+        <div class="h-20 w-20">
+          <img :src="logo" alt="logo" class="cursor-pointer w-full h-full object-contain">
         </div>
-      </div>
-    </div>
-  </div>
-</div>
+        <v-app-bar-title tag="h1" class="text-3xl font-bold flex-initial text-gray-800 ">Admin Side</v-app-bar-title>
+        <div class="relative cursor-pointer mr-[10px] text-[#555]">
+        <v-icon icon="$bellOutline" color="black" ></v-icon>
+        <span class="absolute w-[15px] h-[15px] bg-red-500 text-white text-[10px] rounded-full top-[-5px] right-0 flex justify-center items-center">2</span>
+        </div>
+      </template>
+    </v-toolbar>
+
+  </v-app-bar>
 </template>
 
