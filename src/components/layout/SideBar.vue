51817d8148060bbf5a247cc099b081f973c6a401
Amine derbal <aminedoran31@gmail.com>
2024-03-04 13:11:49 +0100

    Add user and products links

--- src/components/layout/SideBar.vue
+++ src/components/layout/SideBar.vue
@@ -1,16 +1,40 @@
 <template>
-
-  <div class="h-screen bg-gray-100 sticky mt-4">
-    <div class="p-[20px] text-[#555]">
-      <div class="mb-[10px]">
-        <h3 className="text-xs text-gray-500">Dashboard</h3>
-        <ul class="list-none p-[5px]">
-          <router-link to="/"> <li class="p-[5px] cursor-pointer flex items-center rounded-[10px]">
-            <v-icon icon="$home" color="black" ></v-icon>Home
-          </li></router-link>
-        </ul>
+  <v-layout class="flex-initial">
+    <div class="h-screen bg-gray-100 sticky">
+      <div class="p-[20px] text-[#555]">
+        <div class="mb-[10px]">
+          <h3 className="text-xs text-gray-500">Dashboard</h3>
+          <ul>
+            <router-link to="/">
+              <li class="p-[5px] cursor-pointer flex items-center rounded-[10px]">
+                <v-icon icon="$home" color="black"></v-icon>Home
+              </li></router-link
+            >
+          </ul>
+        </div>
+        <div class="mb-[10px]">
+          <h3 className="text-xs text-gray-500">Quick Menu</h3>
+          <ul>
+            <router-link to="/user">
+              <li class="p-[5px] cursor-pointer flex items-center rounded-[10px]">
+                <v-icon icon="$accountMultiple" color="black" /> Users
+              </li></router-link
+            >
+            <router-link to="/product">
+              <li class="p-[5px] cursor-pointer flex items-center rounded-[10px]">
+                <v-icon icon="$packageVariant" color="black" /> Products
+              </li></router-link
+            >
+          </ul>
+        </div>
       </div>
     </div>
-
-  </div>
+  </v-layout>
 </template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 5px;
+}
+</style>
