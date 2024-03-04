d44acfbeed4d11d77ff0974c048b59efae725e1c
Amine derbal <aminedoran31@gmail.com>
2024-03-04 13:12:53 +0100

    Update format file

--- src/components/layout/TopBar.vue
+++ src/components/layout/TopBar.vue
@@ -1,33 +1,33 @@
 <script>
-
 const logo = new URL('@/assets//images/logo.png', import.meta.url).href
 
 export default {
-
   data() {
     return {
       logo
     }
   }
 }
-
 </script>
 
 <template>
-  <v-app-bar height="80" >
-    <v-toolbar color="white" >
-      <template color="white" class="h-full w-full px-[20px] flex items-center justify-between"> 
+  <v-app-bar height="80">
+    <v-toolbar color="white">
+      <template color="white" class="h-full w-full px-[20px] flex items-center justify-between">
         <div class="h-20 w-20">
-          <img :src="logo" alt="logo" class="cursor-pointer w-full h-full object-contain">
+          <img :src="logo" alt="logo" class="cursor-pointer w-full h-full object-contain" />
         </div>
-        <v-app-bar-title tag="h1" class="text-3xl font-bold flex-initial text-gray-800 ">Admin Side</v-app-bar-title>
+        <v-app-bar-title tag="h1" class="text-3xl font-bold flex-initial text-gray-800"
+          >Admin Side</v-app-bar-title
+        >
         <div class="relative cursor-pointer mr-[10px] text-[#555]">
-        <v-icon icon="$bellOutline" color="black" ></v-icon>
-        <span class="absolute w-[15px] h-[15px] bg-red-500 text-white text-[10px] rounded-full top-[-5px] right-0 flex justify-center items-center">2</span>
+          <v-icon icon="$bellOutline" color="black"></v-icon>
+          <span
+            class="absolute w-[15px] h-[15px] bg-red-500 text-white text-[10px] rounded-full top-[-5px] right-0 flex justify-center items-center"
+            >2</span
+          >
         </div>
       </template>
     </v-toolbar>
-
   </v-app-bar>
 </template>
-
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,28 +1,31 @@
 <script>
-import { useUserStore } from '@/stores/userStore';
-import { computed,ref,watch } from 'vue';
+import { useUserStore } from '@/stores/userStore'
+import { computed, ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
 
 export default {
-
   setup() {
     const router = useRouter()
     const userStore = useUserStore()
     const user = ref('')
-    const password = ref('') 
-    const isAdmin =  computed(() => userStore.user.isAdmin)
+    const password = ref('')
+    const isAdmin = computed(() => userStore.user.isAdmin)
 
-   // Check if user is admin and redirect to login page if not
-   watch(isAdmin,(isAdminVal)=>{
-    if(isAdminVal){
-      router.push('/')
+    // Check if user is admin and redirect to login page if not
+    watch(
+      isAdmin,
+      (isAdminVal) => {
+        if (isAdminVal) {
+          router.push('/')
+        }
+      },
+      { immediate: true }
+    )
+
+    // Login admin
+    const handleSubmit = async () => {
+      await userStore.loginAdmin(user.value, password.value)
     }
-   },{immediate:true})
-
-  // Login admin
-   const handleSubmit = async() => {
-     await userStore.loginAdmin(user.value, password.value)
-   }
 
     return {
       handleSubmit,
@@ -30,27 +33,39 @@ export default {
       password
     }
   }
-
 }
 </script>
 
 <template>
-  <div class="mt-10 ">
-<v-container class="text-center md:w-50 h-screen">
-  <v-row justify="center">
-    <v-col cols="12" md="8" sm="6" class="h-100">
-      <v-avatar :style="{ backgroundColor: 'red' }" >
-        <v-icon icon="$lock" color="white" ></v-icon>
-      </v-avatar>
-        <div class="text-h3 mt-4 h-sceen">Login</div>
-        <v-form @submit.prevent>
-          <v-text-field v-model="user" label="username" type="text" prepend-icon="$account" placeholder="admin" required></v-text-field>
-          <v-text-field v-model="password" label="Password" type="password" prepend-icon="$lock" placeholder="admin" required></v-text-field>
-          <v-btn @click="handleSubmit" type="submit" class="w-full" color="primary">Login</v-btn>
-        </v-form>
-    </v-col>
-  </v-row>
-</v-container>
+  <div class="mt-10">
+    <v-container class="text-center md:w-50 h-screen">
+      <v-row justify="center">
+        <v-col cols="12" md="8" sm="6" class="h-100">
+          <v-avatar :style="{ backgroundColor: 'red' }">
+            <v-icon icon="$lock" color="white"></v-icon>
+          </v-avatar>
+          <div class="text-h3 mt-4 h-sceen">Login</div>
+          <v-form @submit.prevent>
+            <v-text-field
+              v-model="user"
+              label="username"
+              type="text"
+              prepend-icon="$account"
+              placeholder="admin"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              label="Password"
+              type="password"
+              prepend-icon="$lock"
+              placeholder="admin"
+              required
+            ></v-text-field>
+            <v-btn @click="handleSubmit" type="submit" class="w-full" color="primary">Login</v-btn>
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
-
 </template>
