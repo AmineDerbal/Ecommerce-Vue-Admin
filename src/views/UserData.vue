cab8a702c339186d997add7ef8e878b141192ab7
youcefRG <youyou.si@outlook.fr>
2024-03-10 15:53:59 +0100

    Create user display view

---
+++ src/views/UserData.vue
@@ -0,0 +1,22 @@
+<script>
+import { useRoute } from 'vue-router'
+import {useUserStore} from '@/stores/userStore'
+import {LoaderComponent} from '@/components'
+
+export default {
+    components:{
+        LoaderComponent
+    },
+
+    setup() {
+        const route = useRoute()
+        const userStore = useUserStore()
+        const userId = route.params.id
+ 
+        return {
+            userStore,
+        }
+    }
+}
+
+</script>
