ee68645f56ebd6d0962d3c1d502a7eb5c45bf0ad
youcefRG <youyou.si@outlook.fr>
2024-03-10 15:52:11 +0100

    Create Loader component

---
+++ src/components/LoaderComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="absolute inset-0 flex justify-center items-center">
+    <div class="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.lds-ring {
+  width: 100px;
+  height: 100px;
+  position: relative;
+  margin: auto;
+}
+
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 100px; /* Adjust based on the size of your spinner */
+  height: 100px; /* Adjust based on the size of your spinner */
+  margin: 8px;
+  border: 8px solid #fff;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: black transparent transparent transparent;
+}
+
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
