f721b266bda4b8db82c31d0197509892a720efcc
AmineDerbal <aminedoran31@gmail.com>
2024-03-21 13:22:56 +0100

    update import and exports

--- src/views/index.js
+++ src/views/index.js
@@ -2,5 +2,6 @@ import HomeView from './HomeView.vue'
 import LoginView from './LoginView.vue'
 import UsersView from './UsersView.vue'
 import UserData from './UserData.vue'
+import CreateUser from './CreateUser.vue'
 
-export { HomeView, LoginView, UsersView, UserData }
+export { HomeView, LoginView, UsersView, UserData, CreateUser }
